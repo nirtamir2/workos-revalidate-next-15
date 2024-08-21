47d992b17ad828cfafb57b6c7d5b0269193a98bd
Nir Tamir <nirtamir2@gmail.com>
2024-08-21 14:08:22 +0300

    styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="py-32">{children}</div>
+      </body>
     </html>
   );
 }
