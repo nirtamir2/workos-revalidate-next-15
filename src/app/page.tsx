077514fdc38fab63756776425b54789b5f4b34f0
Nir Tamir <nirtamir2@gmail.com>
2024-08-26 13:41:42 +0300

    format links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,27 @@ import Link from "next/link";
 export default function Home() {
   return (
     <ul>
-      <Link href="/server-component/query">Server component query</Link>
-      <Link href="/server-component/mutation">Server component mutation</Link>
-      <Link href="/context">context</Link>
-      <Link href="/server-actions">server-actions</Link>
-      <Link href="/server-actions-with-initial-server-state">
-        server-actions-with-initial-server-state
-      </Link>
-      <Link href="/use">use</Link>
+      <li>
+        <Link href="/server-component/query">Server component query</Link>
+      </li>
+      <li>
+        <Link href="/server-component/mutation">Server component mutation</Link>
+      </li>
+      <li>
+        <Link href="/context">context</Link>
+      </li>
+      <li>
+        <Link href="/server-actions">server-actions</Link>
+      </li>
+      <li>
+        <Link href="/server-actions-with-initial-server-state">
+          {" "}
+          server-actions-with-initial-server-state
+        </Link>
+      </li>
+      <li>
+        <Link href="/use">use</Link>
+      </li>
     </ul>
   );
 }
